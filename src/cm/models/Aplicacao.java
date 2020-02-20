fa12a9559f6707dfcd93c7a6e06c5c607e049045
João Victor Vieira de Andrade <joaovictorvieira.23@hotmail.com>
2020-02-19 22:30:51 -0300

    feat: add board

---
+++ src/cm/models/Aplicacao.java
@@ -0,0 +1,12 @@
+package cm.models;
+
+import view.TabuleiroConsole;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 3);
+		new TabuleiroConsole(tabuleiro);
+	}
+}
